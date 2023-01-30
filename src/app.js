7eaaa25b92478dca41bf1578106bd8cb428cc0f9
olimov Amir <olimov.amir@mail.ru>
2023-01-30 13:48:57 +0300

    add file .ico

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ console.log('Amir')
 import './main.scss';
 
 import logo from './assets/mockup/IMAGE5.png';
-
 const logoImage = document.getElementById('logo');
 logoImage.src = logo;
+
+import foto_4 from './assets/mockup/IMAGE4.png';
+const fotoImage_4 = document.getElementById('foto_4');
+fotoImage_4.src = foto_4;
+
+import foto_vector from './assets/mockup/Vector.png';
+const fotoImageVector = document.getElementById('foto_vector');
+fotoImageVector.src = foto_vector;
+
+import foto_vector1 from './assets/mockup/Vector.png';
+const fotoImageVector1 = document.getElementById('foto_vector1');
+fotoImageVector1.src = foto_vector1;
+
+import foto_vector2 from './assets/mockup/Vector.png';
+const fotoImageVector2 = document.getElementById('foto_vector2');
+fotoImageVector2.src = foto_vector2;
+
